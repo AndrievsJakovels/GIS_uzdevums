2d102dbbef378e742ee2caf94c824132941e1d33
andrievs.jakovels@gmail.com <andrievs.jakovels@gmail.com>
2019-07-30 23:17:35 +0300

    labojums

--- GIS/Controllers/HomeController.cs
+++ GIS/Controllers/HomeController.cs
@@ -15,16 +15,11 @@ namespace GIS.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+   
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
     }
 }
